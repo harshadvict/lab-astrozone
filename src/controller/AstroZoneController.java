1233b7585336c75621d0c92b67423330e17d758a
harshadvict <harshadvict@gmail.com>
2020-09-09 21:19:58 +0530

    harsh

--- src/controller/AstroZoneController.java
+++ src/controller/AstroZoneController.java
@@ -1,9 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.User;
-import service.AstroCalculator;
+
 
 @WebServlet(urlPatterns = {"/astrozone"})
 public class AstroZoneController extends HttpServlet {
